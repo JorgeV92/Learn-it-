4daa8482281fdcb8b888239fffc6a674888391cb
Jorge Velez <jorgevmg31@gmail.com>
2024-06-28 21:26:21 -0500

    More code and more

--- Complete/OCaml-Complete/Binary-Tree/binary_tree.ml
+++ Complete/OCaml-Complete/Binary-Tree/binary_tree.ml
@@ -1,3 +1,32 @@
+(* ******************************************************************************************************************************* *)
+                                                      (* Binary search *)
+
+
+let rec binary_search a t l r = 
+  if r = l then 
+    if a.(l) = t then 
+      l 
+    else
+      raise Not_found 
+  else 
+    let m = l + (r - l) / 2 in 
+    if a.(m) > t then 
+      binary_search a t l (m - 1) 
+    else if a.(m) < t then 
+      binary_search a t (m + 1) r
+    else
+      m   
+
+let arr = [|1; 3; 5; 6; 7; 8; 9; 10|]
+
+let () = 
+  Printf.printf "\nBinary search \n"; 
+  let value = binary_search arr 6 0 (Array.length arr - 1) in 
+  Printf.printf "%d\n\n " value 
+
+
+(* ******************************************************************************************************************************* *)
+
 type 'a binary_tree = 
   | Empty 
   | Node of 'a * 'a binary_tree * 'a binary_tree
@@ -44,5 +73,56 @@ let () =
       in
       print_tree tree;
       print_newline ()
-    ) trees 
+    ) trees
 
+(* ******************************************************************************************************************************* *)
+
+type 'a tree = Empty | Node of 'a * 'a tree * 'a tree
+
+let rec preorder f = function 
+    | Empty -> ()
+    | Node (v, l, r) -> f v;
+    preorder f l;
+    preorder f r
+
+let rec inorder f = function 
+    | Empty -> ()
+    | Node (v, l, r) -> 
+    inorder f l; 
+    f v; 
+    inorder f r
+
+let rec postorder f = function
+    | Empty -> ()
+    | Node (v, l ,r) -> 
+      postorder f l;
+      postorder f r;
+      f v
+
+let levelorder f x = 
+  let queue = Queue.create () in 
+  Queue.add x queue; 
+  while not (Queue.is_empty queue) do 
+    match Queue.take queue with 
+      | Empty -> ()
+      | Node (v, l, r) -> f v;
+        Queue.add l queue;
+        Queue.add r queue;
+  done
+
+  let tree =
+    Node (1,
+          Node (2,
+                Node (4,
+                      Node (7, Empty, Empty),
+                      Empty),
+                Node (5, Empty, Empty)),
+          Node (3,
+                Node (6,
+                      Node (8, Empty, Empty),
+                      Node (9, Empty, Empty)),
+                Empty))
+
+let () = 
+  preorder (Printf.printf "%d ") tree;
+  print_newline ();
